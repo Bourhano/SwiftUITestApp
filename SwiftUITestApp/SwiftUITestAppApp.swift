86ea734d859002145e814475b514bcbf65e32001
Bourhan Dernayka <hanodern@gmail.com>
2023-01-12 00:41:46 +0200

    Initial Commit

---
+++ SwiftUITestApp/SwiftUITestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestAppApp.swift
+//  SwiftUITestApp
+//
+//  Created by Bourhan DERNAYKA on 12/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
